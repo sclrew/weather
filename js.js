3e7eb844a65ef31eacc200ac810b3646ae36087f
sclrew <sclrew@yahoo.com>
2021-03-18 18:41:14 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -2,7 +2,7 @@
 
 
 function getUrl(town){
-    let url1 = 'http://api.openweathermap.org/data/2.5/weather?q='
+    let url1 = 'https://api.openweathermap.org/data/2.5/weather?q='
     
     let url2 = '&APPID=d849f74ae0d8e55dd10666a12adfd084'
     
